f2378b05fd7e76d247b66fbbbe45b043e350c18b
Matheus Aguiar <matheusaguiardealmeida@gmail.com>
2024-10-23 17:06:10 -0300

    inicio projeto feira cultural

---
+++ scripts/script.js
@@ -0,0 +1,21 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const button = document.querySelector(".botao-menu");
+    const menuLateral = document.querySelector('.menu-lateral');
+    const conteudo = document.querySelector('.conteudo');
+    const background = document.querySelector('.background');
+
+
+
+    
+    button.addEventListener('click', () => {
+        menuLateral.classList.toggle('ativo');
+        conteudo.classList.toggle('ativo');
+        button.classList.toggle('ativo');
+        background.classList.toggle('ativo')
+        document.body.style.backgroundColor = menuLateral.classList.contains('ativo') ? '#003615' : '#ecf0f1'
+
+      
+    })
+
+    
+});
